dd4886a6c8be239bffa147047cacae0419056f77
acsauk <acsauk@gmail.com>
2016-11-13 17:53:24 +0000

    refactors test to break out assignment of bike info into new function - failing test

--- test/BikeListComponent.spec.js
+++ test/BikeListComponent.spec.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import {expect} from 'chai';
+import sinon from 'sinon';
 
 import BikeListComponent from '../lib/BikeListComponent';
 import BikeComponent from '../lib/BikeComponent';
 
+let wrapper;
+
 describe('<BikeListComponent/>', function() {
+  beforeEach(function() {
+    wrapper = mount(<BikeListComponent/>);
+  });
 
   it('renders null initially and state contains an empty array in bikesList', function() {
-    const wrapper = mount(<BikeListComponent/>);
-
     expect(wrapper.state().bikesList).to.be.instanceof(Array);
     expect(wrapper.state().bikesList.length).to.equal(0);
     expect(wrapper.html()).to.equal(null);
   });
 
-  it('Renders a div that contains a BikeComponent and stores the props values correctly', function() {
-
-    const wrapper = mount(<BikeListComponent/>);
+  it('Renders a div that contains a BikeComponent and calls _constructBikeList', function() {
+    sinon.spy(BikeListComponent.prototype, '_constructBikesList');
 
     wrapper.setState({
       bikesList: [
@@ -28,15 +31,31 @@ describe('<BikeListComponent/>', function() {
       ]
     });
 
-    expect(wrapper.state().bikesList).to.be.instanceof(Array);
-    expect(wrapper.state().bikesList.length).to.equal(1);
     expect(wrapper.find('.bikes-list')).to.have.length(1);
+    expect(BikesListComponent.prototype._constructBikesList.calledOnce).to.equal(true);
+  });
 
-    let bikeComponent = wrapper.childAt(0);
-    let bikeComponentProps = bikeComponent.props();
-
-    expect(bikeComponent.type()).to.equal(BikeComponent);
-    expect(bikeComponentProps.bikeName).to.equal('Litening C:68');
-    expect(bikeComponentProps.bikeDescription).to.equal('The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.');
+  it('_constructBikesList works correctly and can handle multiple bikeComponent objects', function() {
+    wrapper.setState({
+      bikesList: [
+        {
+          bikeName: 'Litening C:68',
+          bikeDescription: 'The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.'
+        },
+        {
+          bikeName: 'Litening C:62',
+          bikeDescription: 'A lightweight, dream bike for ambitious cyclists with a high-quality C:62 carbon frame.'
+        }
+      ]
+    });
+    const result = wrapper.instance()._constructBikesList();
+    expect(result).to.be.instanceof(Array);
+    expect(result.length).to.equal(2);
+    expect(mount(result[0]).type()).to.equal(BikeComponent);
+    expect(result[0].props.bikeName).to.equal('Litening C:68');
+    expect(result[0].props.bikeDescription).to.equal('The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.');
+    expect(mount(result[1]).type()).to.equal(BikeComponent);
+    expect(result[1].props.bikeName).to.equal('Litening C:62');
+    expect(result[1].props.bikeDescription).to.equal('A lightweight, dream bike for ambitious cyclists with a high-quality C:62 carbon frame.');
   });
 });
