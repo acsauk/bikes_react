fcc1303d2f3132b7159be4b610cad81b00f794a0
acsauk <acsauk@gmail.com>
2016-11-14 15:37:37 +0000

    Failing test: displays the bike classes associated with the bike in a paragraph tag

--- test/BikeComponent.spec.js
+++ test/BikeComponent.spec.js
@@ -4,11 +4,17 @@ import {expect} from 'chai';
 
 import BikeComponent from '../lib/BikeComponent';
 
+let wrapper;
+
 describe('<BikeComponent/>', function () {
-  it('should display a bike name and description wrapped in paragraphs in a parent div', function() {
-    const wrapper = shallow(<BikeComponent
+  beforeEach(function() {
+    wrapper = shallow(<BikeComponent
                               bikeName={ 'Litening C:68' }
-                              bikeDescription={ 'The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.' } />);
+                              bikeDescription={ 'The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.' }
+                              bikeClass={ ["endurance", "race"] }/>);
+  });
+
+  it('should display a bike name and description wrapped in paragraphs in a parent div', function() {
     expect(wrapper.type()).to.equal('div');
     expect(wrapper.hasClass('bike')).to.equal(true);
 
@@ -21,5 +27,12 @@ describe('<BikeComponent/>', function () {
     expect(descriptionP.type()).to.equal('p');
     expect(descriptionP.hasClass('bikeDescription')).to.equal(true);
     expect(descriptionP.text()).to.equal('Description: The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.');
+  });
+
+  it('displays the bike classes associated with the bike in a paragraph tag', function() {
+    let classP = wrapper.childAt(2);
+    expect(classP.type()).to.equal('p');
+    expect(classP.hasClass('bikeClass')).to.equal(true);
+    expect(classP.text()).to.equal('Classes: Endurance, Race');
   });
 });
