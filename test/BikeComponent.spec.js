3d5f03e49d4a2c8b9bbe69b80a1d59101ecbebb2
acsauk <acsauk@gmail.com>
2016-11-14 20:17:32 +0000

    Failing test: 'displays the bike image associated with the bike in an img tag'

--- test/BikeComponent.spec.js
+++ test/BikeComponent.spec.js
@@ -12,7 +12,9 @@ describe('<BikeComponent/>', function () {
                               bikeId={ 1 }
                               bikeName={ 'Litening C:68' }
                               bikeDescription={ 'The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.' }
-                              bikeClasses={ ["endurance", "race"] }/>);
+                              bikeClasses={ ["endurance", "race"] }
+                              bikeImage={ "https://www.cube.eu/typo3temp/_processed_/csm_780500_overview_35318ff11c.png" }
+                              />);
   });
 
   it('should display a bike name and description wrapped in paragraphs in a parent div', function() {
@@ -31,11 +33,19 @@ describe('<BikeComponent/>', function () {
   });
 
   it('displays the bike classes associated with the bike in a ul tag', function() {
-    let classP = wrapper.childAt(3);
-    expect(classP.type()).to.equal('ul');
-    expect(classP.hasClass('bikeClass')).to.equal(true);
-    expect(classP.find('ul').children()).to.have.length(2);
-    expect(classP.find('ul').childAt(0).text()).to.equal('endurance ')
-    expect(classP.find('ul').childAt(1).text()).to.equal('race ')
+    let classUl = wrapper.childAt(3);
+    expect(classUl.type()).to.equal('ul');
+    expect(classUl.hasClass('bikeClass')).to.equal(true);
+    expect(classUl.find('ul').children()).to.have.length(2);
+    expect(classUl.find('ul').childAt(0).text()).to.equal('endurance ')
+    expect(classUl.find('ul').childAt(1).text()).to.equal('race ')
+  });
+
+  it('displays the bike image associated with the bike in an img tag', function() {
+    let imageImg = wrapper.childAt(4);
+    let image = imageImg.find('img');
+    expect(imageImg.type()).to.equal('img');
+    expect(imageImg.hasClass('bikeImage')).to.equal(true);
+    expect(image.text()).to.equal("https://www.cube.eu/typo3temp/_processed_/csm_780500_overview_35318ff11c.png")
   });
 });
